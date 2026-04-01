408f219f2b1b4690cade40c6e6024a88b6dfca0d
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-01 13:46:41 +0530

    add all file

---
+++ Recursion/Recursion.java
@@ -0,0 +1,19 @@
+package Recursion;
+import java.util.*;
+public class Recursion-Fact{
+    public static int printFact(int n){
+        if(n==0){
+            return 1;
+        }
+       int fact_nm1 = printFact(n-1);
+       int fact = n * fact_nm1;
+       return fact;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int num = sc.nextInt();
+        int ans = printFact(num);
+        System.out.println(ans);
+        sc.close();
+    }
+}
--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -1,3 +1,4 @@
+package Sorting;
 public class Sorting {
     public static void main(String args[]) {
         int arr[] = {7,8,3,1,2};
