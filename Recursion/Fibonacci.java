d6ce89a71689dc2bc03b1cf20793896aa85a0b8f
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-01 15:30:08 +0530

    add fib file

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,5 +1,18 @@
 package Recursion;
 
 public class Fibonacci {
-    
+    public static int calcFib(int i , int n , int fib){
+        if(i==n){
+            System.out.println(fib);
+            return n;
+        }
+        int fib_np1 = calcFib(n+1,n,fib);
+        int fib_n = n + fib_np1;
+        return fib_n;
+    }
+    public static void main (String args []){
+        int ans = calcFib(0,6,0);
+        System.out.println(ans);
+    }
+
 }
