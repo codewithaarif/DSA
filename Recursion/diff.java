5f9024cce5260305d7af1a7f03ec8e444774840d
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-04 22:16:44 +0530

    add file

---
+++ Recursion/diff.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class diff {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int diff = a - b;
+        System.out.println(diff);
+    }
+    
+}
---
+++ Recursion/mul.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class mul {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
