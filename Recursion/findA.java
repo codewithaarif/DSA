42567680743d0bc2354c49362c813d46eeac1fd3
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-04 23:16:51 +0530

    add file

---
+++ Recursion/findA.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class findA {
+    public static int callA(String str,int n){
+        if(arr[]==a){
+            return arr[];
+        }
+        callA(str.charAt(0),n+1);
+        System.out.println(str.charAt(0));
+    }
+    public static void main(String[] args) {
+        String str = "abaacdaefaah";
+        callA( str,0);
+    }
+}
