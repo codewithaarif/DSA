61d9601e9cda5ca4faf3d31dbafed161b749b13c
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-08 01:33:11 +0530

    add Subset code

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -1,5 +1,28 @@
-package Recursion;
+import java.util.*;
 
 public class Subsets {
-    
+    public static void printSubsets(ArrayList<Integer> subset){
+        for(int i = 0; i < subset.size(); i++){
+            System.out.println(subset.get(i) + "");
+        } System.out.println();
+    }
+    public static void findSubsets( int n , ArrayList<Integer> subset ){
+        if (n == 0) {
+            System.out.println(subset);
+            return;
+        }
+
+        // add hoga
+        subset.add(n);
+        findSubsets(n-1, subset);
+
+        // add nahi hoga
+        subset.remove(subset.size()-1);
+        findSubsets(n-1, subset);
+    }
+    public static void main (String args[]){
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubsets(n, subset);
+    }
 }
