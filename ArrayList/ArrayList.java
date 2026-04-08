2d6b0c4e045efb78b949574688a46fa14981faf8
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-04-08 23:54:58 +0530

    add arraylist code

---
+++ ArrayList/ArrayList.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AddArrayList {
+    public static void main (String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // add element
+        list.add(0);
+        list.add(5);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        // get element
+       int element = list.get(1);
+       System.out.println(element);
+
+       // add element in between  
+       list.add(1,7);
+       System.out.println(list);
+
+       // set element
+       list.set(0,19);
+       System.out.println(list);
+
+       // remove element
+       list.remove(0);
+       System.out.println(list);
+
+       int size = list.size();
+       System.out.println(size);
+
+       // loop
+       for(int i = 0; i < list.size();i++){
+        System.out.print(list.get(i) + " ");
+       } 
+       System.out.println();
+    
+       // sort
+       Collections.sort(list);
+       System.out.println(list);
+    }
+}
